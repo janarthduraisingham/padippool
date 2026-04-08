fdc89a7ede45378a7ad7e27ad37e9555252209c2
janarthduraisingham <51697690+janarthduraisingham@users.noreply.github.com>
2026-04-08 11:07:31 +0100

    Feature/006 neaten logic (#7)
    * add empty object for reactive values
    
    * add empty vector to reactive object for question-answer pair
    
    * add empty vector to reactive object for solution
    
    * add empty string to reactive object for check message
    
    * neaten reactivity logic for check message
    
    * remove comments
    
    * move collation of vocab into function on button press
    
    * add separate reactive objects for question and solution
    
    * add reactive object for solution message
    
    * replace old length-2 vector for question reactive object with new length-1 reactive object
    
    * delete commented out code and whitespace
    
    * comment code
    
    * neaten initialisation of reactive values

--- app.R
+++ app.R
@@ -8,11 +8,9 @@ ui <- page_fluid(
     "What will you dive into today?"
   ),
   
-  
   card(
     textOutput("question"),
     actionButton("question", "Question\nகேல்வி")
-    
     ),
   
   card(
@@ -20,50 +18,46 @@ ui <- page_fluid(
     textInput("user_solution", "Enter solution", "")
   ),
   
-  
   card(
     textOutput("solution"),
     textOutput("user_sol"),
-    textOutput("check"),
+    textOutput("check_message"),
     actionButton("solution", "Our Solution"),
-    
   )
-  
-
-  
 )
 
 server <- function(input, output) {
-  
   set.seed(as.numeric(Sys.time()))
   
   source("R/verbs.R")
   source("R/nouns.R")
   
-  qa <- reactiveVal(c("", "")) 
-  sol <- reactiveVal(c("", ""))
-  check <- reactiveVal("")
+  rv <- reactiveValues(question = "",
+                       solution = "",
+                       solution_message = "",
+                       check_message = "")
   
-  output$check = renderText(check())
+  output$check_message = renderText(rv$check_message)
   output$user_sol<- renderText(input$user_solution)
-  output$question = renderText(qa()[1])
-  output$solution = renderText(qa()[2])
-  
-  questions = c(nouns,
-                verbs)
+  output$question = renderText(rv$question)
+  output$solution = renderText(rv$solution_message)
 
+  # On button press, randomly select and print question-solution pair
   observeEvent(input$question, {
-    check("")
+    
+    questions = c(nouns,
+                  verbs)
+    
     selection = sample(questions, 1)[[1]]
-    qa(c(selection[1], ""))
-    sol(c(selection[1], paste(selection[2], "is one possible solution")))
+    rv$question = selection[1]
+    rv$solution = selection[2]
   })
   
+  # On button press, check user input against solution and print message accordingly
   observeEvent(input$solution, {
-    qa(sol())
+    rv$solution_message = paste(rv$solution, "is one possible solution")
     
-    if (sol()[2] == paste(input$user_solution, "is one possible solution")) {check("Yours is the same!")} else { check("Yours is different")}
-    
+    if (rv$solution == input$user_solution) {rv$check_message = "Yours is the same!"} else {rv$check_message = "Yours is different"}
   })
 }
 
