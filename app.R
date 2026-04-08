5357940f96bb2934095c181fb8dca397e9b20911
Janarth Duraisingham <jduraisingham108@gmail.com>
2026-04-08 11:03:00 +0100

    delete commented out code and whitespace

--- app.R
+++ app.R
@@ -8,11 +8,9 @@ ui <- page_fluid(
     "What will you dive into today?"
   ),
   
-  
   card(
     textOutput("question"),
     actionButton("question", "Question\nகேல்வி")
-    
     ),
   
   card(
@@ -20,21 +18,15 @@ ui <- page_fluid(
     textInput("user_solution", "Enter solution", "")
   ),
   
-  
   card(
     textOutput("solution"),
     textOutput("user_sol"),
     textOutput("check_message"),
     actionButton("solution", "Our Solution"),
-    
   )
-  
-
-  
 )
 
 server <- function(input, output) {
-  
   set.seed(as.numeric(Sys.time()))
   
   source("R/verbs.R")
@@ -45,17 +37,10 @@ server <- function(input, output) {
                        solution_message = "",
                        check_message = "")
   
-  qa <- reactiveVal(c("", "")) 
-  sol <- reactiveVal(c("", ""))
-  check <- reactiveVal("")
-  
   output$check_message = renderText(rv$check_message)
   output$user_sol<- renderText(input$user_solution)
-  #output$question = renderText(qa()[1])
   output$question = renderText(rv$question)
   output$solution = renderText(rv$solution_message)
-  
-  
 
   observeEvent(input$question, {
     
@@ -65,20 +50,12 @@ server <- function(input, output) {
     selection = sample(questions, 1)[[1]]
     rv$question = selection[1]
     rv$solution = selection[2]
-    qa(c(selection[1], ""))
-    sol(c(selection[1], paste(selection[2], "is one possible solution")))
   })
   
   observeEvent(input$solution, {
+    rv$solution_message = paste(rv$solution, "is one possible solution")
     
-    rv$solution_message = paste(rv$solution, "is a possible solution")
-    
-    qa(sol())
-    
-    if (sol()[2] == paste(input$user_solution, "is a possible solution")) {rv$check_message = "Yours is the same!"} else {rv$check_message = "Yours is different"}
-    #if (sol_message == paste(input$user_solution, "is one possible solution")) {rv$check_message = "Yours is the same!"} else {rv$check_message = "Yours is different"}
-    
-    
+    if (rv$solution == input$user_solution) {rv$check_message = "Yours is the same!"} else {rv$check_message = "Yours is different"}
   })
 }
 
