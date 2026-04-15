7d32a789b9cf0bc4a5b50b26b4d0b83e7d1b8730
janarthduraisingham <51697690+janarthduraisingham@users.noreply.github.com>
2026-04-15 17:29:37 +0100

    Feature/047 dative nouns (#54)
    * add more nouns in the social case
    
    * add nouns in the dative case

--- R/nouns_social.R
+++ R/nouns_social.R
@@ -1,3 +1,5 @@
 nouns_social <- list(
-  c("He ate fruit with peel", "தோலோடு பழம் சாப்பிட்டான்")
+  c("He ate fruit with peel", "தோலோடு பழம் சாப்பிட்டான்"),
+  c("He went with son", "மகனோடு போனான்"),
+  c("She painted eye with mascara", "மையால் கண்ணை தீட்டினாள்")
 )
