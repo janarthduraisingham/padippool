da8ba8a671dca00817dc1577519b55f7fb212432
Janarth Duraisingham <jduraisingham108@gmail.com>
2026-04-15 17:29:17 +0100

    add nouns in the dative case

--- R/nouns_dative.R
+++ R/nouns_dative.R
@@ -1,3 +1,5 @@
 nouns_dative <- list(
-  c("", "")
+  c("He went home", "வீட்டுக்குப் போனான்"),
+  c("Cow has four legs ", "மாட்டுக்கு நான்கு கால்கள்"),
+  c("He went to Chennai", "ஞென்னைக்குப் போனான்")
 )
