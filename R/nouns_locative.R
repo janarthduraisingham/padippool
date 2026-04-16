71ce5c74fef84c3c241365ac0c400b0d291fb3b9
Janarth Duraisingham <jduraisingham108@gmail.com>
2026-04-16 12:16:31 +0100

    add examples for nouns in the locative case

--- R/nouns_locative.R
+++ R/nouns_locative.R
@@ -1,3 +1,5 @@
 nouns_locative <- list(
-  c("", "")
+  c("Book is in the house", "புத்தகம் வீட்டில் இருக்கிறது"),
+  c("Book is on the table", "புத்தகம் மேஜைமேல் இருக்கிறது"),
+  c("Book is with him", "பத்தகம் அவனிடம் இருக்கிளது")
 )
