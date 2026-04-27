e596a1fe5e7bdb0a8745ee2ba8e5ca938b88f907
Janarth Duraisingham <jduraisingham108@gmail.com>
2026-04-27 15:38:36 +0100

    prefix nouns in the ablative case

--- R/nouns_ablative.R
+++ R/nouns_ablative.R
@@ -1,3 +1,7 @@
 nouns_ablative <- list(
   c("He is coming from home", "வீட்டிலிருந்து வருக்றான்")
-)
+) %>%
+  map(~ {
+    .x[1] = paste0("[Nouns in the Ablative case] ", .x[1]) # prepend question type
+    .x
+  })
