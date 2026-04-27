92f7c5724a18094367f01307eebbe1b482317da0
Janarth Duraisingham <jduraisingham108@gmail.com>
2026-04-27 15:29:27 +0100

    add prefix for adjectives

--- R/adjectives.R
+++ R/adjectives.R
@@ -4,4 +4,8 @@ adjectives <- list(
   c("flat", "தட்டை"),
   c("round; spherical", "உருண்டை"),
   c("suitable", "தகுந்த")
-)
+) %>%
+  map(~ {
+    .x[1] = paste0("[Adjectives] ", .x[1]) # prepend question type
+    .x
+  })
