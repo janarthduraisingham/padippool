cb14df4a8703041385ca42cba160cddf3d5cb572
Janarth Duraisingham <jduraisingham108@gmail.com>
2026-04-27 15:36:49 +0100

    prefix nouns in the genitive case

--- R/nouns_genitive.R
+++ R/nouns_genitive.R
@@ -2,4 +2,8 @@ nouns_genitive <- list(
   c("Rama's hand", "ராமனின் கை"),
   c("India's capital is Delhi, (dilli)", "இந்தியாவின் தலைநகரம் டில்லி"),
   c("Rama's book is in his house", "ராமனின் புத்தகம் அவனின் வீட்டில் இருக்கிறது")
-)
+) %>%
+  map(~ {
+    .x[1] = paste0("[Nouns in the Genitive case] ", .x[1]) # prepend question type
+    .x
+  })
