a0463e2478f1fc0af0ac6017194615c6f78dd07d
Janarth Duraisingham <jduraisingham108@gmail.com>
2026-04-27 15:32:12 +0100

    prefix pronouns

--- R/prepositions.R
+++ R/prepositions.R
@@ -3,4 +3,8 @@ prepositions <- list(
   c("outside", "வெளியே"),
   c("above; up", "மேலே"),
   c("below; down", "கீழே")
-)
+) %>%
+  map(~ {
+    .x[1] = paste0("[Prepositions] ", .x[1]) # prepend question type
+    .x
+  })
