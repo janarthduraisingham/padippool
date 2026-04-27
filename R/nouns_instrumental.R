6b1a06d4dad21cc2870d15c7ba28598801a41e31
Janarth Duraisingham <jduraisingham108@gmail.com>
2026-04-27 15:36:09 +0100

    prefix nouns in the instrumental case

--- R/nouns_instrumental.R
+++ R/nouns_instrumental.R
@@ -1,4 +1,8 @@
 nouns_instrumental <- list(
   c("He cut with knife", "கத்தியால் வெட்டினான்"),
   c("He wrote with hand", "கையால் எழுதினான்")
-)
+) %>%
+  map(~ {
+    .x[1] = paste0("[Nouns in the Instrumental case] ", .x[1]) # prepend question type
+    .x
+  })
